b33e0005fe59bf9d04044e78fbeda324294b8e3a
Lw中 <1544712020@qq.com>
2020-07-25 17:34:38 +0800

    补充service

---
+++ src/main/java/com/lwz/vblog/service/CategoryService.java
@@ -0,0 +1,43 @@
+package com.lwz.vblog.service;
+
+import com.lwz.vblog.bean.Category;
+import com.lwz.vblog.mapper.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * @author Lw中
+ * @date 2020/7/25 17:29
+ */
+
+@Service
+@Transactional
+public class CategoryService {
+
+    @Autowired
+    CategoryMapper categoryMapper;
+
+    public List<Category> getAllCategories() {
+        return categoryMapper.getAllCategories();
+    }
+
+    public boolean deleteCategoryByIds(String ids) {
+        String[] split = ids.split(",");
+        int result = categoryMapper.deleteCategoryByIds(split);
+        return result == split.length;
+    }
+
+    public int updateCategoryById(Category category) {
+        return categoryMapper.updateCategoryById(category);
+    }
+
+    public int addCategory(Category category) {
+        category.setDate(new Timestamp(System.currentTimeMillis()));
+        return categoryMapper.addCategory(category);
+    }
+
+}
