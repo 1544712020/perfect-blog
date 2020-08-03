5a2669ae4b66281e13081ef7fb46c195e081ec56
Lw中 <1544712020@qq.com>
2020-08-03 23:24:02 +0800

    添加controller

---
+++ src/main/java/com/lwz/vblog/controller/CategoryController.java
@@ -0,0 +1,8 @@
+package com.lwz.vblog.controller;
+
+/**
+ * @author Lw中
+ * @date 2020/8/3 23:22
+ */
+public class CategoryController {
+}
---
+++ src/main/java/com/lwz/vblog/controller/admin/AdminController.java
@@ -0,0 +1,8 @@
+package com.lwz.vblog.controller.admin;
+
+/**
+ * @author Lw中
+ * @date 2020/8/3 23:23
+ */
+public class AdminController {
+}
