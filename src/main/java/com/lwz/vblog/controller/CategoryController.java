2db9380f59f29b3f41b86d6c8300e4cfb8fb4412
Lw中 <1544712020@qq.com>
2020-08-06 21:13:51 +0800

    完成登录注册controller

--- src/main/java/com/lwz/vblog/controller/CategoryController.java
+++ src/main/java/com/lwz/vblog/controller/CategoryController.java
@@ -1,8 +1,80 @@
 package com.lwz.vblog.controller;
 
+import com.lwz.vblog.bean.Category;
+import com.lwz.vblog.bean.RespBean;
+import com.lwz.vblog.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * @author Lw中
  * @date 2020/8/3 23:22
  */
+
+@RestController
+@RequestMapping("/admin/category")
 public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    /**
+     * 获取所有文章类别
+     * @return
+     */
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    /**
+     * 批量删除文章类别
+     * @param ids
+     * @return
+     */
+    @RequestMapping(value = "/{ids}", method = RequestMethod.DELETE)
+    public RespBean deleteById(@PathVariable String ids) {
+        boolean result = categoryService.deleteCategoryByIds(ids);
+        if (result) {
+            return new RespBean("success", "删除成功!");
+        }
+        return new RespBean("error", "删除失败!");
+    }
+
+    /**
+     * 添加新的文章类别
+     * @param category
+     * @return
+     */
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public RespBean addNewCate(Category category) {
+        if ("".equals(category.getCateName()) || category.getCateName() == null) {
+            return new RespBean("error", "请输入栏目名称!");
+        }
+        int result = categoryService.addCategory(category);
+        if (result == 1) {
+            return new RespBean("success", "添加成功!");
+        }
+        return new RespBean("error", "添加失败!");
+    }
+
+    /**
+     * 更新文章类别
+     * @param category
+     * @return
+     */
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    public RespBean updateCate(Category category) {
+        int i = categoryService.updateCategoryById(category);
+        if (i == 1) {
+            return new RespBean("success", "修改成功!");
+        }
+        return new RespBean("error", "修改失败!");
+    }
+
 }
--- src/main/java/com/lwz/vblog/controller/LoginRegController.java
+++ src/main/java/com/lwz/vblog/controller/LoginRegController.java
@@ -1,8 +1,69 @@
 package com.lwz.vblog.controller;
 
+import com.lwz.vblog.bean.RespBean;
+import com.lwz.vblog.bean.User;
+import com.lwz.vblog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Lw中
  * @date 2020/8/3 23:22
+ * 登陆注册Controller
  */
+
+@RestController
 public class LoginRegController {
+
+    @Autowired
+    UserService userService;
+
+    /**
+     * 登陆失败
+     * @return
+     */
+    @RequestMapping("/login_error")
+    public RespBean loginError() {
+        return new RespBean("error", "登录失败!");
+    }
+
+    /**
+     * 登录成功
+     * @return
+     */
+    @RequestMapping("/login_success")
+    public RespBean loginSuccess() {
+        return new RespBean("success", "登录成功!");
+    }
+
+    /**
+     * 如果自动跳转到这个页面，说明用户未登录
+     * @return
+     */
+    @RequestMapping("/login_page")
+    public RespBean loginPage() {
+        return new RespBean("error", "尚未登录，请登录!");
+    }
+
+    /**
+     * 用户注册
+     * @param user
+     * @return
+     */
+    @PostMapping("/reg")
+    public RespBean reg(User user) {
+        int result = userService.reg(user);
+        if (result == 0) {
+            //成功
+            return new RespBean("success", "注册成功!");
+        } else if (result == 1) {
+            return new RespBean("error", "用户名重复，注册失败!");
+        } else {
+            //失败
+            return new RespBean("error", "注册失败!");
+        }
+    }
+
 }
