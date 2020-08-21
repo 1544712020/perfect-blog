c5c6341b1f9d4b12161c5af7de91097c3e17e76e
Lw中 <1544712020@qq.com>
2020-08-21 22:32:39 +0800

    完善SpringSecurity

--- src/main/java/com/lwz/vblog/config/WebSecurityConfig.java
+++ src/main/java/com/lwz/vblog/config/WebSecurityConfig.java
@@ -21,10 +21,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * LWZ TODO : 2020/8/10 SpringSecurity
- * web安全适配器如何使用
- */
-/**
  * @author 15447
  */
 @Configuration
@@ -40,20 +36,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin/category/all").authenticated()
+                .antMatchers("/admin/category/all").authenticated()// 用于给某些路径添加上需要登录或者相关登录的时候才可以访问
                 .antMatchers("/admin/**","/reg").hasRole("超级管理员")///admin/**的URL都需要有超级管理员角色，如果使用.hasAuthority()方法来配置，需要在参数中加上ROLE_,如下.hasAuthority("ROLE_超级管理员")
                 .anyRequest().authenticated()//其他的路径都是登录后即可访问
-                .and().formLogin().loginPage("/login_page").successHandler(new AuthenticationSuccessHandler() {
-            @Override
-            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                httpServletResponse.setContentType("application/json;charset=utf-8");
-                PrintWriter out = httpServletResponse.getWriter();
-                out.write("{\"status\":\"success\",\"msg\":\"登录成功\"}");
-                out.flush();
-                out.close();
-            }
-        })
-                .failureHandler(new AuthenticationFailureHandler() {
+                .and()
+                .formLogin()
+                .loginPage("/login_page")
+                .successHandler(new AuthenticationSuccessHandler() {//  登录成功处理方式
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        httpServletResponse.setContentType("application/json;charset=utf-8");
+                        PrintWriter out = httpServletResponse.getWriter();
+                        out.write("{\"status\":\"success\",\"msg\":\"登录成功\"}");
+                        out.flush();
+                        out.close();
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {// 登录失败处理方式
                     @Override
                     public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
                         httpServletResponse.setContentType("application/json;charset=utf-8");
@@ -62,9 +61,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         out.flush();
                         out.close();
                     }
-                }).loginProcessingUrl("/login")
-                .usernameParameter("username").passwordParameter("password").permitAll()
-                .and().logout().permitAll().and().csrf().disable().exceptionHandling().accessDeniedHandler(getAccessDeniedHandler());
+                })
+                .loginProcessingUrl("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .csrf().disable()
+                .exceptionHandling()
+                .accessDeniedHandler(getAccessDeniedHandler());
+        /**
+         * LWZ TODO : 2020/8/10 SpringSecurity-accessDeniedHandler()
+         * accessDeniedHandler(getAccessDeniedHandler()有何用
+         */
     }
 
     @Override
