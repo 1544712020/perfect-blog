ccf2f24547167a02a21ad395272a6d7f6fe0a172
Lw中 <1544712020@qq.com>
2020-08-27 20:39:19 +0800

    解决todo

--- src/main/java/com/lwz/vblog/config/MyPasswordEncoder.java
+++ src/main/java/com/lwz/vblog/config/MyPasswordEncoder.java
@@ -5,11 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.DigestUtils;
 
 /**
- * LWZ TODO : 2020/7/29 SpringSecurity
- * 此类属于SpringSecurity中的加密类，用于对用户密码进行加密
- */
-
-/**
+ * 此类实现了passwordEncoder：主要用于对密码加密
  * @author Lw中
  * @date 2020/7/29 19:20
  */
@@ -17,14 +13,26 @@ import org.springframework.util.DigestUtils;
 @Component
 public class MyPasswordEncoder implements PasswordEncoder {
 
+    /**
+     *此类将用户传入的明文密码进行加密，返回密文
+     * @param rawPassword
+     * @return
+     */
     @Override
     public String encode(CharSequence rawPassword) {
         return DigestUtils.md5DigestAsHex(rawPassword.toString().getBytes());
     }
 
+    /**
+     * 用户登录时，将用户输入的密码和数据库中的密文密码校对
+     * @param rawPassword
+     * @param encodedPassword
+     * @return
+     */
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
         return encodedPassword.equals(DigestUtils.md5DigestAsHex(rawPassword.toString().getBytes()));
     }
+
 
 }
