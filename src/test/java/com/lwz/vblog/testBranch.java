3344cede5caf414b39b254f1dceb83366edf786a
Lw中 <1544712020@qq.com>
2020-08-31 10:00:24 +0800

    testBranch

---
+++ src/test/java/com/lwz/vblog/testBranch.java
@@ -0,0 +1,13 @@
+package com.lwz.vblog;
+
+/**
+ * @author Lw中
+ * @date 2020/8/31 9:59
+ */
+public class testBranch {
+
+    public static void main(String[] args) {
+        System.out.println("testBranch!");
+    }
+
+}
