ffe5d613efe0da53d98db81039c4414c03709587
Lw中 <1544712020@qq.com>
2020-09-23 15:57:21 +0800

    添加SwaggerConfig

---
+++ src/main/java/com/lwz/vblog/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.lwz.vblog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Swagger配置类
+ * @author Lw中
+ * @date 2020/8/24 16:20
+ */
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                /* 配置要生成接口文档的包名 */
+                .apis(RequestHandlerSelectors.basePackage("com.lwz.vblog.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(" RESTful APIs")
+                .description("RESTful APIs")
+                .termsOfServiceUrl("http://localhost:8080/")
+                .contact("long")
+                .version("1.0")
+                .build();
+    }
+
+
+}
