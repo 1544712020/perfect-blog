da829b8dceeda30aae3958e12f46752ceae5944d
Lw中 <1544712020@qq.com>
2020-09-28 15:45:35 +0800

    完善Swagger整合

--- src/main/java/com/lwz/vblog/VblogApplication.java
+++ src/main/java/com/lwz/vblog/VblogApplication.java
@@ -3,6 +3,7 @@ package com.lwz.vblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author 15447
  */
 @SpringBootApplication
+@EnableSwagger2
 @EnableScheduling
 public class VblogApplication {
 
