8f9d77927a7ad8566ef88948ac5bd43504529eff
Lw中 <1544712020@qq.com>
2020-10-29 15:24:20 +0800

    添加ServiceTest测试类测试缓存

---
+++ src/test/java/com/lwz/vblog/ServiceTest.java
@@ -0,0 +1,29 @@
+package com.lwz.vblog;
+
+import com.lwz.vblog.bean.Article;
+import com.lwz.vblog.service.ArticleService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Lw中
+ * @date 2020/10/29 15:15
+ */
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ServiceTest {
+
+    @Autowired
+    ArticleService articleService;
+
+    @Test
+    public void testService() {
+        Article article = articleService.getArticleById(108l);
+        System.out.println(article.getTitle());
+    }
+
+}
