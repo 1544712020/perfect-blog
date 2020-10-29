77b9673db0551d2facadfb3f384dc3232dd9ccb0
Lw中 <1544712020@qq.com>
2020-10-29 15:11:28 +0800

    添加redis配置类

---
+++ src/main/java/com/lwz/vblog/config/RedisConfig.java
@@ -0,0 +1,102 @@
+package com.lwz.vblog.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.lang.reflect.Method;
+
+/**
+ * Redis配置类
+ * @author Lw中
+ * @date 2020/9/4 13:44
+ */
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+    /**
+     * 在缓存对象集合中，缓存是以key-value形式保存的
+     * 如果没有指定缓存中的key，springboot就会通过SimpleGenerator来生成key
+     * @return
+     */
+    @Override
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            // 定义缓存数据key的生成策略
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    /**
+     * 缓存管理器：开启spring对缓存的支持
+     * SuppressWarnings：告诉编译器忽略指定的警告，不用在编译完成后出现警告信息
+     * @param connectionFactory
+     * @return
+     */
+    @SuppressWarnings("rawtypes")
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        // 通过Redis依赖中的connectionFactory来创建缓存管理器
+        RedisCacheManager cacheManager = RedisCacheManager.create(connectionFactory);
+        //返回缓存管理器
+        return cacheManager;
+    }
+
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(factory);
+//        return redisTemplate;
+//    }
+//
+//    @Bean
+//    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+//        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+//        stringRedisTemplate.setConnectionFactory(factory);
+//        return stringRedisTemplate;
+//    }
+
+    /**
+     * 如果没有这个bean，那么redis可视化工具中的中文会出现乱码
+     * @param factory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        // 通过connectionFactory来设置如何存储entries中的中文字符
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+
+}
