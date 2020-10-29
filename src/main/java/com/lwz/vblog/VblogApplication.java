4c1f25caa7b3f48f1fd7e6628e4d51fb1a732862
Lw中 <1544712020@qq.com>
2020-10-29 15:12:46 +0800

    完善注解

--- src/main/java/com/lwz/vblog/VblogApplication.java
+++ src/main/java/com/lwz/vblog/VblogApplication.java
@@ -2,6 +2,7 @@ package com.lwz.vblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableScheduling
+@EnableCaching
 public class VblogApplication {
 
     public static void main(String[] args) {
