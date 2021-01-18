d7d316d3879bc9bf83dbbdee30d706e7ad8a549a
Lw中 <1544712020@qq.com>
2021-01-18 22:14:38 +0800

    完善注释

--- src/main/java/com/lwz/vblog/mapper/TagsMapper.java
+++ src/main/java/com/lwz/vblog/mapper/TagsMapper.java
@@ -14,18 +14,33 @@ import java.util.List;
 @Mapper
 public interface TagsMapper {
 
+    /**
+     * 通过id删除单个标签信息
+     * @param aid
+     * @return
+     */
     int deleteTagsByAid(Long aid);
 
+    /**
+     * 批量删除标签信息
+     * @param ids
+     * @return
+     */
     int deleteTagsByAid1(@Param("ids") String[] ids);
 
     /**
-     * 添加标签
+     * 批量添加标签
      *
      * @param tags
      * @return
      */
     int saveTags(@Param("tags") String[] tags);
 
+    /**
+     * 添加单个标签信息
+     * @param tagName
+     * @return
+     */
     int saveTags1(Tags tagName);
 
     /**
@@ -45,8 +60,17 @@ public interface TagsMapper {
      */
     int saveTags2ArticleTags(@Param("tagIds") List<Long> tagIds, @Param("aid") Long aid);
 
+    /**
+     * 查询所有标签
+     * @return
+     */
     List<Tags> getAllTags();
 
+    /**
+     * 通过标签id更新标签信息
+     * @param tags
+     * @return
+     */
     int updateTagById(Tags tags);
 
 }
